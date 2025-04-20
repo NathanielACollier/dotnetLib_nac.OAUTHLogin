5d5262fbed3e54c2eac6d100f85f5cd5e307ffd1
Nathaniel Collier <drxs3v3n@gmail.com>
2025-04-20 18:35:32 -0500

    Add a UI Test App so we can do the tests on macos. + Not sure it works good yet.   It's a work in progress

---
+++ TestApp_UITester/Program.cs
@@ -0,0 +1,15 @@
+﻿using repos=TestApp_UITester.repos;
+
+var log = new nac.Logging.Logger();
+
+nac.Logging.Appenders.ColoredConsole.Setup();
+        
+try
+{
+    nac.Forms.UITesterApp.TestApp.Run(typeof(repos.TestFunctions.TestFunctions));
+            
+
+}catch(Exception ex)
+{
+    log.Fatal($"App Exception occured: {ex}");
+}
---
+++ TestApp_UITester/repos/TestFunctions/TestFunctions.cs
@@ -0,0 +1,71 @@
+using nac.Forms;
+
+namespace TestApp_UITester.repos.TestFunctions;
+
+public class TestFunctions
+{
+    private static nac.Logging.Logger log = new();
+    
+    public static void MicrosoftLogin(Form f)
+    {
+        f.Text("Token")
+            .TextBoxFor("tokenField");
+        
+        var microsoftSettings = nac.OAUTHLogin.repositories.VendorOAUTHSettings.GetMicrosoftOAUTHSettings();
+        microsoftSettings.ClientId = lib.settings.OAUTH_Microsoft_ClientId;
+        microsoftSettings.ClientSecret = lib.settings.OAUTH_Microsoft_ClientSecret;
+        microsoftSettings.Scope = "https://graph.microsoft.com/.default";
+
+        Task.Run(async () =>
+        {
+            string token = await nac.OAUTHLogin.OAUTH.GetAuthTokenViaDefaultBrowser(microsoftSettings);
+
+            if (string.IsNullOrWhiteSpace(token) || token.Length < 25)
+            {
+                log.Error("Microsoft Login - Token generation failed");
+            }
+            
+            f.setModelValue("tokenField", token);
+            
+
+        });
+        
+    }
+
+
+    public static void GoogleLogin(Form f)
+    {
+        f.Text("Token")
+            .TextBoxFor("tokenField");
+        
+        var oauthSettings = nac.OAUTHLogin.repositories.VendorOAUTHSettings.GetGoogleOAUTHSettings();
+        oauthSettings.ClientId = lib.settings.OAUTH_Google_ClientId;
+        oauthSettings.ClientSecret = lib.settings.OAUTH_Google_ClientSecret;
+        oauthSettings.Scope = "email";
+
+        Task.Run(async () =>
+        {
+            string token = await nac.OAUTHLogin.OAUTH.GetAuthTokenViaDefaultBrowser(oauthSettings);
+
+            if (string.IsNullOrWhiteSpace(token) || token.Length < 25)
+            {
+                log.Error("Microsoft Login - Token generation failed");
+            }
+            
+            f.setModelValue("tokenField", token);
+            
+
+        });
+    }
+
+
+
+    public static void ShowGoogleHomepage(Form f)
+    {
+        var win = nac.OAUTHLogin.repositories.PhotinoBrowserRepo.OpenAtUrl("https://www.google.com/");
+        
+        win.WaitForClose();
+    }
+    
+    
+}
